0b4ad7cab97c8efe4d66d725a8be80c1e5cc640e
Migueltsw <136586406+Migueltsw@users.noreply.github.com>
2023-10-06 10:06:04 -0300

    Add files via upload

---
+++ pages/router.php
@@ -0,0 +1,14 @@
+<?php
+
+ $rotas = [
+   '/' => '/pages/home.html',
+   '/dashboard' => '/pages/dashboard.php',
+   '/users/create' => '/pages/users/registro.php',
+   '/users/login' => '/pages/users/login.html',
+   '/logout' => '/pages/users/logout.php',
+   '/books/create' => '/pages/books/create.php',
+   '/books/store' => '/pages/books/store.php',
+   '/books/index' => '/pages/books/index.php'
+ ];
+
+?>
